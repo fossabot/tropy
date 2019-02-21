6445a77f329f19d032fa605b76391b3ee187b5f7
Flachware <johannes.krtek@flachware.com>
2019-02-21 11:30:05 +0100

    Add static test content and styles.

--- src/windows/print.js
+++ src/windows/print.js
@@ -2,4 +2,18 @@
 
 const { $, html } = require('../dom')
 
-html($('main'), 'Just do it right. Make it perfect and real.')
+/* eslint-disable no-multi-str */
+html($('main'), '\
+  <div class="page">\
+    <img src="../static/images/landscape.png" width="4000" height="2000">\
+  </div>\
+  <div class="page">\
+    <img src="../static/images/portrait.png" width="2000" height="4000">\
+  </div>\
+  <div class="page">\
+    <img src="../static/images/landscape-sm.png" width="400" height="200">\
+  </div>\
+  <div class="page">\
+    <img src="../static/images/portrait-sm.png" width="200" height="400">\
+  </div>\
+')
